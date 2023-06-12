2e4c11de9f35a8e687d2c6672b732fb0a4453bd7
poteresting <poteresting@gmail.com>
2023-06-13 00:19:54 +0300

    fix(home): text spacing

--- app/src/pages/home.rs
+++ app/src/pages/home.rs
@@ -11,7 +11,7 @@ pub fn HomePage(cx: Scope) -> impl IntoView {
             <div class="bg-white py-24 sm:py-32">
                 <div class="mx-auto max-w-7xl px-6 lg:px-8">
                     <div class="mx-auto max-w-2xl lg:text-center">
-                        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        <p class="mt-2 text-3xl font-bold text-gray-900 sm:text-4xl">
                             "Trading Platform built using..."
                         </p>
                     </div>
