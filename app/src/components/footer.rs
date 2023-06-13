4af93c7dcf3c6abfc6410f8db5b920339a8a5a2e
Arina Solovjova <arina@eosfintek.com>
2023-06-13 22:44:58 +0300

    style: set margin top 0

--- app/src/components/footer.rs
+++ app/src/components/footer.rs
@@ -85,7 +85,7 @@ pub fn Footer(cx: Scope) -> impl IntoView {
                                 </a>
                             </li>
                         </ul>
-                        <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">"&copy; 2023 Roberts Ivanovs. All rights reserved."</p>
+                        <p class="text-xs leading-5 text-gray-400 md:order-1 mt-0">"&copy; 2023 Roberts Ivanovs. All rights reserved."</p>
                     </div>
                 </div>
             </div>
